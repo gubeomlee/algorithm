79710f3b418a762507017cfb8e7e19db4ffca7e1
gubeomlee <118784674+gubeomlee@users.noreply.github.com>
2023-07-16 16:34:24 +0900

    [D4] Title: 창용 마을 무리의 개수, Time: 218 ms, Memory: 43,176 KB -BaekjoonHub

--- SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
+++ SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
@@ -1,13 +1,20 @@
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Queue;
 import java.util.Scanner;
 
 class Solution {
-	public static void dfs(ArrayList<ArrayList<Integer>> matrix, boolean[] is_checked, int node) {
-		is_checked[node] = true; //현재 노드 방문 처리 
-		// 현재 노드와 연결된 노드들에 대해 재귀적으로 방문 
-		for (int adjacentNode : matrix.get(node)) {
-			if (!is_checked[adjacentNode]) {
-				dfs(matrix, is_checked, adjacentNode);
+	public static void bfs(ArrayList<ArrayList<Integer>> matrix, boolean[] is_checked, int node) {
+		Queue<Integer> deque = new ArrayDeque<>();
+		deque.add(node);
+
+		while (!deque.isEmpty()) {
+			int cur = deque.poll();
+			if (!is_checked[cur]) {
+				is_checked[cur] = true; // 현재 노드 방문 처리
+				for (int adjacentNode : matrix.get(cur)) {
+					deque.add(adjacentNode); // 현재 노드와 연결된 노드들 큐 추가 
+				}
 			}
 		}
 	}
@@ -16,14 +23,14 @@ class Solution {
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
 		for (int test_case = 1; test_case <= T; test_case++) {
-			int n = sc.nextInt(); // 노드의 개수 
-			int m = sc.nextInt(); // 간선의 개수 
-			
-			ArrayList<ArrayList<Integer>> matrix = new ArrayList<>(); // 인접 리스트 그래프 
+			int n = sc.nextInt(); // 노드의 개수
+			int m = sc.nextInt(); // 간선의 개수
+
+			ArrayList<ArrayList<Integer>> matrix = new ArrayList<>(); // 인접 리스트 그래프
 			for (int i = 0; i <= n; i++) {
 				matrix.add(new ArrayList<Integer>());
 			}
-			// 간선 정보 입력 및 인접 리스트 그래프 구성 
+			// 간선 정보 입력 및 인접 리스트 그래프 구성
 			for (int i = 0; i < m; i++) {
 				int num1 = sc.nextInt();
 				int num2 = sc.nextInt();
@@ -37,7 +44,7 @@ class Solution {
 			for (int i = 1; i <= n; i++) {
 				if (!is_ckecked[i]) {
 					result++;
-					dfs(matrix, is_ckecked, i);
+					bfs(matrix, is_ckecked, i);
 				}
 			}
 			System.out.printf("#%d %d\n", test_case, result);
