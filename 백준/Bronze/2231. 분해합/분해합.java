299b77e150229f32c3dbe4926537ecf8ab1d2021
gubeomlee <118784674+gubeomlee@users.noreply.github.com>
2023-07-19 09:25:30 +0900

    [Bronze II] Title: 분해합, Time: 212 ms, Memory: 17676 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.java
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class Main {
+	// 주어진 숫자의 각 자리수를 더해주는 메서드 
 	public static int func(int num) {
 		int result = num;
 		for (char ch : Integer.toString(num).toCharArray()) {
@@ -12,6 +13,7 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int num = sc.nextInt();
+		// 각 자리의 최댓값은 9이므로 "수의 자리수*10"만큼 작은 수보다 작다면 정답이 될 수 없다.
 		for (int i = num - Integer.toString(num).length() * 10; i < num; i++) {
 			if (num == func(i)) {
 				System.out.println(i);
