fb79bb289cccc277dc6e7f504f8490602ccd5c51
gubeomlee <118784674+gubeomlee@users.noreply.github.com>
2023-07-22 21:53:56 +0900

    [Bronze I] Title: 최소공배수, Time: 356 ms, Memory: 22320 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class Main {
+	// 유클리드 호재법을 이용하여 최대공약수 구함.
+	public static int gcd(int a, int b) {
+		while (b != 0) {
+			int temp = a;
+			a = b;
+			b = temp % b;
+		}
+		return a;
+	}
+
+	public static int lcm(int a, int b) {
+		return a * b / gcd(a, b);
+	}
+
+	public static void main(String args[]) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for (int test_case = 0; test_case < T; test_case++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+
+			int result = lcm(Math.max(a, b), Math.min(a, b));
+			System.out.println(result);
+		}
+		sc.close();
+	}
+}
