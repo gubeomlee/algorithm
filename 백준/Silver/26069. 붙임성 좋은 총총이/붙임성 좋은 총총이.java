4376e44ded8e9a485132b9a998f72848dcecec0e
gubeomlee <118784674+gubeomlee@users.noreply.github.com>
2023-07-24 17:23:43 +0900

    [Silver IV] Title: 붙임성 좋은 총총이, Time: 312 ms, Memory: 20784 KB -BaekjoonHub

---
+++ 백준/Silver/26069. 붙임성 좋은 총총이/붙임성 좋은 총총이.java
@@ -0,0 +1,22 @@
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		int len = sc.nextInt();
+		sc.nextLine();
+
+		HashSet<String> set = new HashSet<>();
+		set.add("ChongChong");
+		for (int i = 0; i < len; i++) {
+			String[] name = sc.nextLine().split(" ");
+			if (set.contains(name[0]) || set.contains(name[1])) {
+				set.add(name[0]);
+				set.add(name[1]);
+			}
+		}
+		System.out.println(set.size());
+		sc.close();
+	}
+}
