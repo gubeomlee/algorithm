cc8e72ebe67e0ff37d71a416381f3c7f53712899
gubeomlee <118784674+gubeomlee@users.noreply.github.com>
2023-07-29 11:58:38 +0900

    [Silver III] Title: N과 M (1), Time: 632 ms, Memory: 69256 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -5,16 +5,21 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-	public static void backtrack(List<Integer> arr, int depth, Deque<Integer> temp, List<Deque<Integer>> result) {
+	private static StringBuilder sb = new StringBuilder();
+
+	public static void backtrack(List<Integer> arr, int depth, Deque<Integer> temp) {
 		if (depth == 0) {
-			result.add(new LinkedList<>(temp));
+			for (int num : temp) {
+				sb.append(num + " ");
+			}
+			sb.append("\n");
 		} else {
 			for (int i = 0; i < arr.size(); i++) {
 				// 숫자를 선택하고, 선택한 숫자를 제외한 나머지 숫자들의 리스트를 만든다.
 				List<Integer> subList = new ArrayList<Integer>(arr.subList(0, i));
 				subList.addAll(arr.subList(i + 1, arr.size()));
 				temp.add(arr.get(i)); // 선택한 숫자를 temp에 추가한다.
-				backtrack(subList, depth - 1, temp, result);
+				backtrack(subList, depth - 1, temp);
 				temp.pollLast(); // 백트래킹
 			}
 		}
@@ -29,16 +34,9 @@ public class Main {
 			arr.add(i);
 		}
 
-		List<Deque<Integer>> result = new ArrayList<>();
-		backtrack(arr, m, new LinkedList<>(), result);
+		backtrack(arr, m, new LinkedList<>());
 
-		for (Deque<Integer> que : result) {
-			StringBuilder sb = new StringBuilder();
-			for (int num : que) {
-				sb.append(num + " ");
-			}
-			System.out.println(sb);
-		}
+		System.out.println(sb);
 		sc.close();
 	}
 }
