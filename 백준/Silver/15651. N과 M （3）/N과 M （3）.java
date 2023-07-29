44564557f68fabbc9891da6b030e058b3fc5aede
gubeomlee <118784674+gubeomlee@users.noreply.github.com>
2023-07-29 11:49:26 +0900

    [Silver III] Title: N과 M (3), Time: 812 ms, Memory: 240368 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+	private static StringBuilder sb = new StringBuilder();
+	public static void backtrack(List<Integer> arr, int depth, Deque<Integer> temp) {
+		if (depth == 0) {
+			for (int num : temp) {
+				sb.append(num + " ");
+			}
+			sb.append("\n");
+		} else {
+			for (int i = 0; i < arr.size(); i++) {
+				temp.add(arr.get(i)); // 선택한 숫자를 temp에 추가한다.
+				backtrack(arr, depth - 1, temp);
+				temp.pollLast(); // 백트래킹
+			}
+		}
+	}
+
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		List<Integer> arr = new ArrayList<>();
+		for (int i = 1; i < n + 1; i++) {
+			arr.add(i);
+		}
+
+		backtrack(arr, m, new LinkedList<>());
+
+		System.out.println(sb);
+		sc.close();
+	}
+}
