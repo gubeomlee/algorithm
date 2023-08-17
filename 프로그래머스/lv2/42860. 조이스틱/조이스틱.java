07aaca40a4b101f24b6f32bf3d95f39323867bab
gubeomlee <118784674+gubeomlee@users.noreply.github.com>
2023-08-18 08:21:12 +0900

    [level 2] Title: 조이스틱, Time: 0.29 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/lv2/42860. 조이스틱/조이스틱.java
+++ 프로그래머스/lv2/42860. 조이스틱/조이스틱.java
@@ -1,29 +1,38 @@
 import java.util.Stack;
 
 public class Solution {
+	// 조이스틱 좌우 조작 횟수 계산하는 메서드
 	public static void getVisitedCnt(String name, int idx, int[] result) {
 		Stack<Character> stack = new Stack<>();
-
+		// 반대 방향으로 이동한 위치가 idx다.
+		// 시작점부터 idx 이전까지 스택에 추가한다.
 		for (int i = 0; i < idx; i++) {
 			stack.add(name.charAt(i));
 		}
+		// idx 이전부터 시작점까지 스택에 추가한다.
 		for (int i = idx - 2; i >= 0; i--) {
 			stack.add(name.charAt(i));
 		}
+		// 문자열 끝부터 idx까지 스택에 추가한다.
 		for (int i = name.length() - 1; i >= idx; i--) {
 			stack.add(name.charAt(i));
 		}
+		// 연속되는 'A' 제거한다.
 		while (stack.peek() == 'A') {
 			stack.pop();
 		}
+		// 스택의 길이가 좌우 이동횟수가 된다.
 		int cnt = stack.size() - 1;
+		// idx가 0인 경우 이 값은 스택의 top에 위치한다.
+		// idx가 0이고 그 값이 'A'인 경우 스택에서 제거되는 문제가 있어 좌우 이동 횟수를 보정해야 한다.
 		if (idx == 0 && name.charAt(0) == 'A') {
 			cnt++;
 		}
-
+		// 좌우 이동 횟수를 더 작은 값으로 갱신한다.
 		result[0] = Math.min(result[0], cnt);
 	}
 
+	// 조이스틱 상하 조작 횟수 계산하는 메서드
 	public static int joystick(String name) {
 		int result = 0;
 		for (int i = 0; i < name.length(); i++) {
@@ -33,31 +42,31 @@ public class Solution {
 	}
 
 	public static int solution(String name) {
-		int joystickResult = joystick(name);
-
-		int[] result = { name.length() };
-
-		if (joystickResult == 0) {
-			result[0] = 0;
+		int[] visitedCnt = { name.length() };
+		int joystickCnt = joystick(name);
+		if (joystickCnt == 0) { // 조이스틱 상하 조작 횟수가 0이라면
+			visitedCnt[0] = 0; // 조이스틱 좌우 조작 횟수도 0이다.
 		} else {
-			for (int i = 0; i < name.length(); i++) {
-				if (i == 0 || name.charAt(i) == 'A') {
-					getVisitedCnt(name, i, result);
-				}
-			}
-
+			// 첫 이동이 왼쪽인 경우에 사용할 문자열을 생성한다.
 			StringBuilder sb = new StringBuilder();
 			sb.append(name.charAt(0));
 			for (int i = name.length() - 1; i >= 1; i--) {
 				sb.append(name.charAt(i));
 			}
-
+			// 첫 이동이 오른쪽일 때 좌우 조작 횟수를 계산한다.
+			for (int i = 0; i < name.length(); i++) {
+				// 시작 위치 또는 현재 위치가 'A'일때 반대 방향으로 이동한다.
+				if (i == 0 || name.charAt(i) == 'A') {
+					getVisitedCnt(name, i, visitedCnt);
+				}
+			}
+			// 첫 이동이 왼쪽일 때 좌우 조작 횟수를 계산한다.
 			for (int i = 0; i < name.length(); i++) {
 				if (i == 0 || sb.toString().charAt(i) == 'A') {
-					getVisitedCnt(sb.toString(), i, result);
+					getVisitedCnt(sb.toString(), i, visitedCnt);
 				}
 			}
 		}
-		return joystickResult + result[0];
+		return joystickCnt + visitedCnt[0];
 	}
 }
