067aafb0ced073e04b4515b0ced86cdb75ac6270
gubeomlee <118784674+gubeomlee@users.noreply.github.com>
2023-08-22 21:46:04 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문3, Time: 424 ms, Memory: 83,472 KB -BaekjoonHub

--- SWEA/D3/1230. ［S／W 문제해결 기본］ 8일차 － 암호문3/［S／W 문제해결 기본］ 8일차 － 암호문3.java
+++ SWEA/D3/1230. ［S／W 문제해결 기본］ 8일차 － 암호문3/［S／W 문제해결 기본］ 8일차 － 암호문3.java
@@ -1,5 +1,153 @@
-import java.util.LinkedList;
 import java.util.Scanner;
+
+class Node {
+	public int data;
+	public Node pre;
+	public Node next;
+
+	public Node() {
+	}
+
+	public Node(int data) {
+		this.data = data;
+	}
+
+	@Override
+	public String toString() {
+		return "Node [data=" + data + "]";
+	}
+}
+
+class DoublyLinkedList {
+	private Node head;
+	private Node tail;
+	private int size;
+
+	public DoublyLinkedList() {
+	}
+
+	public Node get(int idx) {
+		if (idx < 0 || size <= idx) {
+			return null;
+		}
+
+		if (idx < size / 2) {
+			Node cur = head;
+			for (int i = 0; i < idx; i++) {
+				cur = cur.next;
+			}
+			return cur;
+		} else {
+			Node cur = tail;
+			for (int i = size - 1; i > idx; i--) {
+				cur = cur.pre;
+			}
+			return cur;
+		}
+
+	}
+
+	public void addFirst(int data) {
+		Node node = new Node(data);
+		node.next = head;
+
+		if (head != null) {
+			head.pre = node;
+		}
+
+		head = node;
+		size++;
+
+		if (head.next == null) {
+			tail = head;
+		}
+	}
+
+	public void addLast(int data) {
+		if (size == 0) {
+			addFirst(data);
+			return;
+		}
+
+		Node node = new Node(data);
+
+		tail.next = node;
+		node.pre = tail;
+		tail = node;
+		size++;
+	}
+
+	public void add(int idx, int data) {
+		if (idx < 0 || size <= idx) {
+			return;
+		}
+		if (idx == 0) {
+			addFirst(data);
+			return;
+		}
+		if (idx == size) {
+			addLast(data);
+			return;
+		}
+
+		Node node = new Node(data);
+		Node pre = get(idx - 1);
+		Node next = pre.next;
+
+		node.pre = pre;
+		node.next = next;
+
+		pre.next = node;
+		next.pre = node;
+
+		size++;
+	}
+
+	public int removeFirst() {
+		int data = head.data;
+
+		head = head.next;
+		size--;
+
+		return data;
+	}
+
+	public int removeLast() {
+		if (size == 0) {
+			return 0;
+		}
+
+		int data = tail.data;
+
+		tail = tail.pre;
+		size--;
+
+		return data;
+	}
+
+	public int remove(int idx) {
+		if (idx < 0 || size <= idx) {
+			return 0;
+		}
+		if (idx == 0) {
+			removeFirst();
+		}
+		if (idx == size) {
+			removeLast();
+		}
+
+		Node removeNode = get(idx);
+		Node pre = get(idx - 1);
+		Node next = pre.next;
+
+		pre.next = next;
+		next.pre = pre;
+
+		size--;
+
+		return removeNode.data;
+	}
+}
 
 public class Solution {
 	public static void main(String args[]) throws Exception {
@@ -7,14 +155,13 @@ public class Solution {
 		int T = 10;
 		for (int t = 1; t <= T; t++) {
 			int n = sc.nextInt();
-			LinkedList<Integer> list = new LinkedList<>();
+			DoublyLinkedList list = new DoublyLinkedList();
 			for (int i = 0; i < n; i++) {
 				list.addLast(sc.nextInt());
 			}
-		
-			int m = sc.nextInt(); // 연산의 개수 입력
+			int m = sc.nextInt();
 			for (int i = 0; i < m; i++) {
-				String com = sc.next(); // 연산 타입 ("I", "D", "A")
+				String com = sc.next();
 				if (com.equals("I")) {
 					int idx = sc.nextInt();
 					int len = sc.nextInt();
@@ -25,19 +172,19 @@ public class Solution {
 					int idx = sc.nextInt();
 					int len = sc.nextInt();
 					for (int j = idx; j < idx + len; j++) {
-						list.remove(j); // 삭제 위치에 있는 요소 삭제
+						list.remove(j);
 					}
 				} else if (com.equals("A")) {
 					int len = sc.nextInt();
 					for (int j = 0; j < len; j++) {
-						list.addLast(sc.nextInt()); // 리스트 끝에 요소 추가
+						list.addLast(sc.nextInt());
 					}
 				}
 			}
 
 			System.out.printf("#%d ", t);
 			for (int i = 0; i < 10; i++) {
-				System.out.printf("%d ", list.get(i));
+				System.out.printf("%d ", list.get(i).data);
 			}
 			System.out.println();
 		}
