4b3ffa44acda4b079ac1c7b49de94de3fc6924e3
gubeomlee <118784674+gubeomlee@users.noreply.github.com>
2023-08-25 12:26:09 +0900

    [D3] Title: 원재의 메모리 복구하기, Time: 128 ms, Memory: 20,132 KB -BaekjoonHub

--- SWEA/D3/1289. 원재의 메모리 복구하기/원재의 메모리 복구하기.java
+++ SWEA/D3/1289. 원재의 메모리 복구하기/원재의 메모리 복구하기.java
@@ -8,21 +8,13 @@ public class Solution {
 			char[] arr = sc.next().toCharArray();
 
 			int result = 0; // 결과값
-			int temp = 0; // 임시값
 			for (int i = 0; i < arr.length; i++) {
-				if (arr[i] == '1' && result == 0) { // 첫번째 1을 만날 때
+				if (result != 0 && arr[i] != arr[i - 1]) { // 이전값과 다른 값이 나왔을 때
+					result += 1;
+				}
+				if (result == 0 && arr[i] == '1') { // 첫번째 1을 만날 때
 					result = 1;
 				}
-				if (arr[i] == '1' && result != 0) { // 0다음 연속된 1이 나오는 경우
-					result += temp;
-					temp = 0;
-				}
-				if (arr[i] == '0' && result != 0) { // 0이 나온경우 
-					temp = 2;
-				}
-			}
-			if (temp != 0) { // 0으로 끝난 경우
-				result++;
 			}
 			System.out.printf("#%d %d\n", t, result);
 		}
