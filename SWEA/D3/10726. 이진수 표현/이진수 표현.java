013560e5975df15111c41d77a0d78602f33c8dd4
gubeomlee <118784674+gubeomlee@users.noreply.github.com>
2023-08-29 11:22:48 +0900

    [D3] Title: 이진수 표현, Time: 466 ms, Memory: 81,964 KB -BaekjoonHub

---
+++ SWEA/D3/10726. 이진수 표현/이진수 표현.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for (int t = 1; t <= T; t++) {
+			int n = sc.nextInt();
+			int m = sc.nextInt();
+
+			String result = "ON";
+			for (int i = 0; i < n; i++) {
+				// i번째 비트가 0이면
+				if ((m & (int) (Math.pow(2, i))) == 0) {
+					result = "OFF"; // 스위치 OFF
+				}
+			}
+			System.out.printf("#%d %s\n", t, result);
+		}
+		sc.close();
+	}
+}
