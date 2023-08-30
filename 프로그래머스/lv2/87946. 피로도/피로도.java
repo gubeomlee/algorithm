9d9d52281e12ecd01445c6e453e1ec382beb232e
gubeomlee <118784674+gubeomlee@users.noreply.github.com>
2023-08-30 15:46:45 +0900

    [level 2] Title: 피로도, Time: 0.86 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/lv2/87946. 피로도/피로도.java
+++ 프로그래머스/lv2/87946. 피로도/피로도.java
@@ -3,17 +3,17 @@ import java.util.List;
 
 public class Solution {
 	public static void backtrack(int[][] matrix, List<Integer> list, int k, int cnt, int depth, int[] result) {
-		if (k < 0) {
+		if (k < 0) { // 탐험 불가능한 상황
 			result[0] = Math.max(result[0], cnt - 1);
-		} else if (depth == 0) {
+		} else if (depth == 0) { // 모든 던전을 탐험한 경우
 			result[0] = Math.max(result[0], cnt);
 		} else {
 			for (int i = 0; i < list.size(); i++) {
 				List<Integer> subList = new ArrayList<>(list.subList(0, i));
 				subList.addAll(list.subList(i + 1, list.size()));
-				if (k >= matrix[list.get(i)][0]) {
+				if (k >= matrix[list.get(i)][0]) { // 탐험 가능한 경우
 					backtrack(matrix, subList, k - matrix[list.get(i)][1], cnt + 1, depth - 1, result);
-				} else {
+				} else { // 탐험 불가능한 경우
 					result[0] = Math.max(result[0], cnt);
 				}
 			}
