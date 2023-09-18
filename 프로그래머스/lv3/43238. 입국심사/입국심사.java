87c74b6f18329e4f236c2c2af57abaa9da8467c9
gubeomlee <118784674+gubeomlee@users.noreply.github.com>
2023-09-18 22:03:41 +0900

    [level 3] Title: 입국심사, Time: 0.09 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43238. 입국심사/입국심사.java
@@ -0,0 +1,40 @@
+public class Solution {
+	// 최댓값을 찾는 메서드 
+	public static long getMax(int n, int[] times) {
+		long max = 1;
+		while (true) {
+			long temp = 0;
+			for (int i = 0; i < times.length; i++) {
+				temp += max / times[i];
+			}
+			if (temp < n) {
+				max *= 2L;
+			} else {
+				break;
+			}
+		}
+		return max;
+	}
+	// 이진탐색을 통해 최적 시간을 찾는 메서드 
+	public static long solution(int n, int[] times) {
+		long result = 0;
+		long max = getMax(n, times);
+		long min = max / 2;
+
+		while (min <= max) {
+			long mid = (min + max) / 2;
+			long temp = 0;
+			for (int i = 0; i < times.length; i++) {
+				temp += mid / times[i];
+			}
+
+			if (temp >= n) {
+				max = mid - 1;
+				result = mid;
+			} else {
+				min = mid + 1;
+			}
+		}
+		return result;
+	}
+}
