aaa471fd3e28a4877ac0acba36411a7b4d5282e9
gubeomlee <118784674+gubeomlee@users.noreply.github.com>
2023-09-21 15:20:52 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 931 ms, Memory: 126,888 KB -BaekjoonHub

--- SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
@@ -1,17 +1,8 @@
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Comparator;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Solution {
-	public static int setFind(int[] set, int num) {
-		if (set[num] != num) {
-			set[num] = setFind(set, set[num]);
-		}
-		return set[num];
-	}
-
 	public static void main(String[] args) throws IOException {
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
@@ -29,39 +20,42 @@ public class Solution {
 				idx[i][1] = Integer.parseInt(st2.nextToken());
 			}
 
-			// 간선 가중치를 구한다.
-			int temp = 0;
-			double[][] node = new double[(n * n - n) / 2][3];
+			double[][] matrix = new double[n][n];
 			for (int i = 0; i < n; i++) {
 				for (int j = i + 1; j < n; j++) {
-					node[temp][0] = i;
-					node[temp][1] = j;
-					node[temp++][2] = Math
+					matrix[i][j] = matrix[j][i] = Math
 							.sqrt(Math.pow(idx[i][0] - idx[j][0], 2) + Math.pow(idx[i][1] - idx[j][1], 2));
+
 				}
 			}
-			// 가중치를 기준으로 정렬한다.
-			Arrays.sort(node, new Comparator<double[]>() {
-				@Override
-				public int compare(double[] o1, double[] o2) {
-					return Double.compare(o1[2], o2[2]);
-				}
-			});
 
+			double[][] prim = new double[n][2];
+			for (int i = 0; i < n; i++) {
+				prim[i][1] = Double.MAX_VALUE;
+			}
+
+			prim[0][1] = 0;
 			double result = 0;
+			for (int i = 0; i < n; i++) {
+				int index = -1;
+				double min = Double.MAX_VALUE;
 
-			int[] set = new int[(n * n - n) / 2 + 1];
-			for (int i = 0; i <= (n * n - n) / 2; i++) {
-				set[i] = i;
-			}
+				for (int j = 0; j < n; j++) {
+					if (prim[j][0] == 0 && prim[j][1] < min) {
+						min = prim[j][1];
+						index = j;
+					}
+				}
 
-			for (int i = 0; i < (n * n - n) / 2; i++) {
-				if (setFind(set, (int) node[i][0]) != setFind(set, (int) node[i][1])) {
-					result += Math.pow(node[i][2], 2);
-					set[setFind(set, (int) node[i][1])] = setFind(set, (int) node[i][0]);
+				prim[index][0] = 1;
+				result += Math.pow(prim[index][1], 2);
+
+				for (int j = 0; j < n; j++) {
+					if (prim[j][0] == 0 && matrix[index][j] != 0 && prim[j][1] > matrix[index][j]) {
+						prim[j][1] = matrix[index][j];
+					}
 				}
 			}
-
 			System.out.printf("#%d %d\n", t, Math.round(result * e));
 		}
 		sc.close();
