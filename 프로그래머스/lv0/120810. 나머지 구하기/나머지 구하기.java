1eafa845d985e76608a00e0430f5da83e809c0b4
gubeomlee <118784674+gubeomlee@users.noreply.github.com>
2023-09-27 09:29:59 +0900

    [level 0] Title: 나머지 구하기, Time: 0.01 ms, Memory: 72.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120810. 나머지 구하기/나머지 구하기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int getRemain(int num1, int num2){
+        return num1 % num2; 
+    }
+    
+    public int solution(int num1, int num2) {
+        int answer = getRemain(num1, num2);
+        return answer;
+    }
+}
