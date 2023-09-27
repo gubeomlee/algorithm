1abb492bf273735cfa0154096506ff92790f41de
gubeomlee <118784674+gubeomlee@users.noreply.github.com>
2023-09-27 10:23:22 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.02 ms, Memory: 83.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int getQuadrant(int[] dot) {
+        if(dot[0] > 0 && dot[1] > 0) {
+            return 1; 
+        } else if (dot[0] < 0 && dot[1] > 0) {
+            return 2; 
+        } else if (dot[0] < 0 && dot[1] < 0) {
+            return 3; 
+        } else {
+            return 4;
+        }
+    }
+    
+    public int solution(int[] dot) {
+        int answer = getQuadrant(dot);
+        return answer;
+    }
+}
