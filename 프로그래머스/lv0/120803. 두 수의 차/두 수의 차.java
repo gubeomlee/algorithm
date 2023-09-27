81dae4977d02b99e99ac03aca7693b0f11be8911
gubeomlee <118784674+gubeomlee@users.noreply.github.com>
2023-09-27 09:28:14 +0900

    [level 0] Title: 두 수의 차, Time: 0.02 ms, Memory: 79.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120803. 두 수의 차/두 수의 차.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int getMinus(int num1, int num2) {
+        return num1 - num2; 
+    }
+    
+    public int solution(int num1, int num2) {
+        int answer = getMinus(num1, num2);
+        return answer;
+    }
+}
