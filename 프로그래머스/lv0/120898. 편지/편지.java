a3eb9b75424b83c84d91d3f94153f08ad7cd423d
gubeomlee <118784674+gubeomlee@users.noreply.github.com>
2023-09-27 10:50:53 +0900

    [level 0] Title: 편지, Time: 0.02 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120898. 편지/편지.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int getLen(String message) {
+        return message.length() * 2; 
+    }
+    
+    public int solution(String message) {
+        int answer = getLen(message);
+        return answer;
+    }
+}
