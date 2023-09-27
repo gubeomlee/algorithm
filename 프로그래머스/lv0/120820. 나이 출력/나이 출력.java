d1583570be915f2fb6e1ec7aa3d099d21dc1128c
gubeomlee <118784674+gubeomlee@users.noreply.github.com>
2023-09-27 09:31:02 +0900

    [level 0] Title: 나이 출력, Time: 0.01 ms, Memory: 70.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120820. 나이 출력/나이 출력.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int getYear(int age) {
+        return 2022 - age + 1;
+    }
+    
+    public int solution(int age) {
+        int answer = getYear(age);
+        return answer;
+    }
+}
