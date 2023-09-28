3bd5d28ed673010b768a906ae5e18dc28337f850
gubeomlee <118784674+gubeomlee@users.noreply.github.com>
2023-09-28 18:02:44 +0900

    [unrated] Title: 공백으로 구분하기 1, Time: 0.02 ms, Memory: 79.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181869. 공백으로 구분하기 1/공백으로 구분하기 1.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String[] getStr(String str) {
+        String[] arr = str.split(" "); 
+        return arr; 
+    }
+    
+    public String[] solution(String my_string) {
+        String[] answer = getStr(my_string);
+        return answer;
+    }
+}
