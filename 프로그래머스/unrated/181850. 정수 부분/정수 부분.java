81c12bd0533e8640e9ea6623c77aac5b74ce1760
gubeomlee <118784674+gubeomlee@users.noreply.github.com>
2023-09-28 15:23:42 +0900

    [unrated] Title: 정수 부분, Time: 0.01 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181850. 정수 부분/정수 부분.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int getInt(double flo) {
+        return (int) flo; 
+    }
+    
+    public int solution(double flo) {
+        int answer = getInt(flo);
+        return answer;
+    }
+}
