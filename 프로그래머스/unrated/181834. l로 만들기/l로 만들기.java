46959cfb45714255b6184ba9f5c636c920021f54
gubeomlee <118784674+gubeomlee@users.noreply.github.com>
2023-09-28 22:56:23 +0900

    [unrated] Title: l로 만들기, Time: 7.41 ms, Memory: 85.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181834. l로 만들기/l로 만들기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String getStr(String str) {
+        StringBuilder sb = new StringBuilder(); 
+        for(int i=0 ; i<str.length() ; i++) {
+            if(str.charAt(i) < 'l') {
+                sb.append('l'); 
+            } else {
+                sb.append(str.charAt(i)); 
+            }
+        }
+        return sb.toString(); 
+    }
+    
+    public String solution(String myString) {
+        String answer = getStr(myString);
+        return answer;
+    }
+}
