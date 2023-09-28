fc3018c8e15ff1955fd3d3c88e3a7a6505ec6b0e
gubeomlee <118784674+gubeomlee@users.noreply.github.com>
2023-09-28 18:12:11 +0900

    [unrated] Title: 대문자로 바꾸기, Time: 0.04 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181877. 대문자로 바꾸기/대문자로 바꾸기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String getStr(String str) {
+        return str.toUpperCase(); 
+    }
+    
+    public String solution(String myString) {
+        String answer = getStr(myString);
+        return answer;
+    }
+}
