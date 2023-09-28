47563d7fc5037b2ef8bfeeebd1259560e1619d78
gubeomlee <118784674+gubeomlee@users.noreply.github.com>
2023-09-28 15:28:26 +0900

    [unrated] Title: 문자열로 변환, Time: 0.02 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181845. 문자열로 변환/문자열로 변환.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String getStr(int n) {
+        return Integer.toString(n); 
+    }
+    
+    public String solution(int n) {
+        String answer = getStr(n);
+        return answer;
+    }
+}
