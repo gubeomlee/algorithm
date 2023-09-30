7242d3235ad9f9f372efffc9be48d7f0fcc8ed02
gubeomlee <118784674+gubeomlee@users.noreply.github.com>
2023-09-30 13:43:40 +0900

    [level 2] Title: 이름에 el이 들어가는 동물 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv2/59047. 이름에 el이 들어가는 동물 찾기/이름에 el이 들어가는 동물 찾기.sql
+++ 프로그래머스/lv2/59047. 이름에 el이 들어가는 동물 찾기/이름에 el이 들어가는 동물 찾기.sql
@@ -1,5 +1,5 @@
 -- 코드를 입력하세요
 SELECT animal_id, name 
 from animal_ins
-where (name like '%el%' or name like '%EL%' or name like '%El%' or name like '%eL%') and animal_type = 'Dog'
-order by name; 
+where lower(name) like '%el%' and animal_type = 'Dog'
+order by name;
