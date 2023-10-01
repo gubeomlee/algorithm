1fd5d8a2ce86aa95cdd483643d66a2b5efe491da
gubeomlee <118784674+gubeomlee@users.noreply.github.com>
2023-10-01 12:07:23 +0900

    [level 0] Title: 숫자 찾기, Time: 0.03 ms, Memory: 77.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int getIdx(int num, int k) {
+        String str = Integer.toString(num); 
+        for(int i=0 ; i<str.length() ; i++) {
+            if(str.charAt(i) - '0' == k) {
+                return i + 1; 
+            }
+        }
+        
+        return -1; 
+    }
+    
+    public int solution(int num, int k) {
+        int answer = getIdx(num, k);
+        return answer;
+    }
+}
