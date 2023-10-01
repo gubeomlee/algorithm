256e46609a7574aa34d2460b7af97626cc9b38d2
gubeomlee <118784674+gubeomlee@users.noreply.github.com>
2023-10-01 18:02:00 +0900

    [level 2] Title: DATETIME에서 DATE로 형 변환, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv2/59414. DATETIME에서 DATE로 형 변환/DATETIME에서 DATE로 형 변환.sql
+++ 프로그래머스/lv2/59414. DATETIME에서 DATE로 형 변환/DATETIME에서 DATE로 형 변환.sql
@@ -1,4 +1,4 @@
 -- 코드를 입력하세요
-SELECT animal_id, name, to_char(datetime, 'yyyy-mm-dd') 날짜 
-from animal_ins
+SELECT animal_id, name, to_char(datetime, 'yyyy-mm-dd') datetime
+from animal_ins 
 order by animal_id; 
