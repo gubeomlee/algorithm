6ca38ac49d2167bfc6bfe3940b97cfaabb83fc3c
gubeomlee <118784674+gubeomlee@users.noreply.github.com>
2023-10-01 11:41:58 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.01 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int getCnt(int n) {
+        int cnt = 1;
+        if(n % 6 == 0) {
+            n /= 6; 
+            cnt *= n;
+            return cnt; 
+        }        
+        if(n % 2 == 0) {
+            n /= 2; 
+        }
+        if(n % 3 == 0) {
+            n /= 3; 
+        }
+        
+        return cnt * n; 
+    }
+    
+    public int solution(int n) {
+        int answer = getCnt(n);
+        return answer;
+    }
+}
