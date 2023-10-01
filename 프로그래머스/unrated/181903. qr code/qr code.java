baa287ab1a9c394dd14b3b8d42d07502572b1bbf
gubeomlee <118784674+gubeomlee@users.noreply.github.com>
2023-10-01 21:48:27 +0900

    [unrated] Title: qr code, Time: 0.04 ms, Memory: 69 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181903. qr code/qr code.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String getStr(int q, int r, String code) {
+        StringBuilder sb = new StringBuilder(); 
+        for(int i=r ; i<code.length() ; i+=q) {
+            sb.append(code.charAt(i)); 
+        }
+        return sb.toString(); 
+    }
+    
+    public String solution(int q, int r, String code) {
+        String answer = getStr(q, r, code);
+        return answer;
+    }
+}
