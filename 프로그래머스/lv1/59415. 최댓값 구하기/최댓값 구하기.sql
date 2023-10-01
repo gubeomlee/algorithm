c4cdaf3d3d68c4191d5796ec2aa0ffda90cd1f00
gubeomlee <118784674+gubeomlee@users.noreply.github.com>
2023-10-01 16:52:26 +0900

    [level 1] Title: 최댓값 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv1/59415. 최댓값 구하기/최댓값 구하기.sql
+++ 프로그래머스/lv1/59415. 최댓값 구하기/최댓값 구하기.sql
@@ -1,8 +1,3 @@
 -- 코드를 입력하세요
-SELECT datetime
-from (
-    select datetime 
-    from animal_ins
-    order by datetime desc
-)
-where rownum = 1; 
+SELECT max(datetime) datetime 
+from animal_ins; 
