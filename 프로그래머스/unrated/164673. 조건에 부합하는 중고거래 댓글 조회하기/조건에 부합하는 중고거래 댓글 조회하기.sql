6d2a7ba32c0c5f900d40bc665986b938eba5f4e7
gubeomlee <118784674+gubeomlee@users.noreply.github.com>
2023-10-01 17:07:03 +0900

    [unrated] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
+++ 프로그래머스/unrated/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -1,5 +1,6 @@
 -- 코드를 입력하세요
-SELECT b.title title, b.board_id board_id, r.reply_id reply_id, r.writer_id writer_id, r.contents contents, to_char(r.created_date, 'yyyy-mm-dd') created_date
-from used_goods_board b, used_goods_reply r
-where b.board_id = r.board_id and extract(year from b.created_date) = 2022 and extract(month from b.created_date) = 10
-order by r.created_date, b.title;
+SELECT b.title, b.board_id, r.reply_id, r.writer_id, r.contents, to_char(r.created_date, 'yyyy-mm-dd') created_date
+from used_goods_board b join used_goods_reply r 
+on b.board_id = r.board_id
+where extract(year from b.created_date) = 2022 and extract(month from b.created_date) = 10
+order by created_date, b.title; 
