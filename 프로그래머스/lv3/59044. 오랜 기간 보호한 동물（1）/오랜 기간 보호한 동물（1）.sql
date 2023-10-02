197c44a2c1ef0e3bb5ed94e90448732a55d6a16c
gubeomlee <118784674+gubeomlee@users.noreply.github.com>
2023-10-02 20:22:34 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -0,0 +1,10 @@
+-- 코드를 입력하세요
+SELECT name, datetime
+from (
+    SELECT i.name, i.datetime
+    from animal_ins i left outer join animal_outs o
+    on i.animal_id = o.animal_id
+    where o.datetime is null
+    order by datetime
+)
+where rownum <= 3; 
