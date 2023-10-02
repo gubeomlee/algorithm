5939092777574a4c976cad8eb3cc8309cf90aa00
gubeomlee <118784674+gubeomlee@users.noreply.github.com>
2023-10-02 13:32:40 +0900

    [unrated] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -0,0 +1,6 @@
+-- 코드를 입력하세요
+SELECT '/home/grep/src/' || b.board_id || '/' || f.file_id || f.file_name || f.file_ext file_path
+from used_goods_board b join used_goods_file f
+    on b.board_id = f.board_id
+where views = (select max(views) from used_goods_board)
+order by f.file_id desc; 
