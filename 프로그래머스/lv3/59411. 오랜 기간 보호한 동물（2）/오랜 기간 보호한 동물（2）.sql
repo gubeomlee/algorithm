836c3cc9a41b7013c70e44d9291fc7cb43d87c9d
gubeomlee <118784674+gubeomlee@users.noreply.github.com>
2023-10-03 11:46:21 +0900

    [level 3] Title: 오랜 기간 보호한 동물(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
+++ 프로그래머스/lv3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
@@ -1,9 +1,9 @@
 -- 코드를 입력하세요
 SELECT animal_id, name
-from(
-    select i.animal_id, i.name
-    from animal_ins i join animal_outs o
-    on i.animal_id = o.animal_id
-    order by (o.datetime - i.datetime) desc
+from (
+    select i.animal_id, i.name 
+    from animal_ins i join animal_outs o 
+    on i.animal_id = o.animal_id 
+    order by (o.datetime - i.datetime) desc  
 )
 where rownum <= 2; 
