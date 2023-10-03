006190abd4f1bae2d279f83ced353963093723d1
gubeomlee <118784674+gubeomlee@users.noreply.github.com>
2023-10-03 11:43:34 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
+++ 프로그래머스/lv3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -1,9 +1,9 @@
 -- 코드를 입력하세요
-SELECT p.id, p.name, p.host_id
+SELECT p.id, p.name, p.host_id 
 from places p join (
     select host_id
     from places
-    group by host_id
+    group by host_id 
     having count(*) > 1
 ) c
 on p.host_id = c.host_id
